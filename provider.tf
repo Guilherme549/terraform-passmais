7e3daf600778be16a92191ab0d70d38a582c5c7c
guilherme <guilhermevs54@gmail.com>
2025-09-20 17:42:57 -0300

    Adding provider backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "6.12.0"
     }
   }
+
+  backend "s3" {
+    bucket = "falana-variaveis-teste1"
+    key = "terraform-state/passmais/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
